84592ca94927bf1993922fd0b2cdd6ccbb56b414
Nicolle <nicollepossop@icloud.com>
2026-04-13 17:02:55 -0500

    Creating Guest class, with it´s attributes and one method

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -6,8 +6,19 @@ from app.services.util import (generate_unique_id, date_lower_than_today_error,
     reservation_not_found_error, guest_not_found_error, room_not_available_error,
     room_not_found_error, room_already_exists_error)
 
-
+from dataclasses import dataclass
 # TODO: Implement Guest class here
+
+class Guest:
+    REGULAR: str = "regular"
+    VIP: str = "vip"
+
+    name: str
+    email: str
+    type: str = REGULAR
+
+   def __str__(self):
+       return f"Guest: {self.name} ({self.email} of type {self.type})"
 
 
 # TODO: Implement Reservation class here
